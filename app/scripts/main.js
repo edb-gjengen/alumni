ecf42d11eec07245973b17231df6d0ee9b9ebeee
misund <thomas@hemmeligadresse.com>
2014-04-25 19:10:14 +0200

    Showing spinner while waiting.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,11 @@ console.log('\'Allo \'Allo!');
 $(function() {
     $('form').on('submit', function() {
         event.preventDefault();
+
+	// Display a spinner
+	$('.interest').html('<div id="floatingCirclesG"><div class="f_circleG" id="frotateG_01"></div><div class="f_circleG" id="frotateG_02"></div><div class="f_circleG" id="frotateG_03"></div><div class="f_circleG" id="frotateG_04"></div><div class="f_circleG" id="frotateG_05"></div><div class="f_circleG" id="frotateG_06"></div><div class="f_circleG" id="frotateG_07"></div><div class="f_circleG" id="frotateG_08"></div></div>');
+
+	// Save and load
         $.post(
             'http://alumni.studentersamfundet.no/async.php',
             {
