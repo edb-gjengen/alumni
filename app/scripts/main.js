6e659bb41d98ce9fe6572b44e1eb0e3678013885
misund <thomas@hemmeligadresse.com>
2014-04-25 19:36:07 +0200

    Fixing ugly bug that makes empty registrations.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,17 +7,19 @@ $(function() {
         event.preventDefault();
 
 	// Display a spinner
-	$('.interest').html('<div id="floatingCirclesG"><div class="f_circleG" id="frotateG_01"></div><div class="f_circleG" id="frotateG_02"></div><div class="f_circleG" id="frotateG_03"></div><div class="f_circleG" id="frotateG_04"></div><div class="f_circleG" id="frotateG_05"></div><div class="f_circleG" id="frotateG_06"></div><div class="f_circleG" id="frotateG_07"></div><div class="f_circleG" id="frotateG_08"></div></div>');
+	$('.interest').append('<div id="floatingCirclesG"><div class="f_circleG" id="frotateG_01"></div><div class="f_circleG" id="frotateG_02"></div><div class="f_circleG" id="frotateG_03"></div><div class="f_circleG" id="frotateG_04"></div><div class="f_circleG" id="frotateG_05"></div><div class="f_circleG" id="frotateG_06"></div><div class="f_circleG" id="frotateG_07"></div><div class="f_circleG" id="frotateG_08"></div></div>');
 
 	// Save and load
+	var toPost = {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            division: $('#division').val(),
+            years: $('#years').val()
+        };
+	console.log(toPost);
         $.post(
             'http://alumni.studentersamfundet.no/async.php',
-            {
-                name: $('#name').val(),
-                email: $('#email').val(),
-                division: $('#division').val(),
-                years: $('#years').val()
-            },
+            toPost,
             function(data){
                 console.log('Successfully posted sumtin.');
                 console.log(data);
